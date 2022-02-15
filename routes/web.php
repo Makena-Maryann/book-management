e2d3266d5c015686e43a13ea4d252c36f1b60334
Makena-Maryann <maryann.makena00@gmail.com>
2022-02-15 12:46:31 +0300

    Adds a model, migration, controller and route resource for the Book

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('books', BookController::class);
