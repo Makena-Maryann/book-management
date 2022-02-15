e97fe680cc23e0b580003b6ca8b6b883fe3d0c40
Makena-Maryann <maryann.makena00@gmail.com>
2022-02-15 13:56:19 +0300

    Adds a model, migration, controller and route resource for the Author

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,4 @@ Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
